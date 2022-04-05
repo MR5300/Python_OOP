9e3678c04bd373188863d64be94b34452d21572b
MR5300 <regulamarcin1@gmail.com>
2022-04-05 23:45:17 +0200

    Klasa łatwy

--- repo/Zadania kontrolne/Klasa_latwy.py
+++ repo/Zadania kontrolne/Klasa_latwy.py
@@ -1,5 +1,13 @@
 class Osoba:
-    def __init__(self):
+    def __init__(self, imie, nazwisko, wiek, hobby):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.wiek = 
+        self.wiek = wiek
+        self.hobby = hobby
+
+osoba = Osoba('Jan', 'Kowalski', 16, 'Sport')
+
+print('Imie:', osoba.imie)
+print('Nazwisko:', osoba.nazwisko)
+print('Wiek:', osoba.wiek)
+print('Hobby:', osoba.hobby)
